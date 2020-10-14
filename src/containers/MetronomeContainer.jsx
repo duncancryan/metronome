c0fc0c3c20e74b0e3676b31cddf59569f42039c2
Duncan Ryan <duncancryan1@gmail.com>
2020-10-14 11:05:05 +0100

    focussing on one metronome to start, deleted double container structure

---
+++ src/containers/MetronomeContainer.jsx
@@ -0,0 +1,15 @@
+import React, {Component, Fragment} from 'react'
+
+export default class MetronomeContainer extends Component{
+
+render(){
+    return(
+        <Fragment>
+            <div> I am a metronome!  </div> 
+
+        </Fragment>
+
+    )
+}
+
+} 
